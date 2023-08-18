131884725f780bdf8709ca9296f4a738b2ef5854
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-08-19 01:04:06 +0200

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,25 +1,20 @@
-def call(String stageName){
+def call(String stageName) {
+  def mavenHome = tool name: "maven3.9.4", type: "maven"
+  def mavenCMD = "${mavenHome}/bin/mvn"
 
-  if ("${stageName}" == "Clone")
-  {
+  if (stageName == "Clone") {
     sh "git clone https://github.com/CHAFAH/GROUP-PROJECT.git"
   }
-  else if ("${stageName}" == "Build")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
+  else if (stageName == "Build") {
     sh "${mavenCMD} clean package"
   }
-  else if ("${stageName}" == "SonarQube Report")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
+  else if (stageName == "SonarQube Report") {
     sh "${mavenCMD} sonar:sonar"
   }
-  else if ("${stageName}" == "Upload to Nexus")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
+  else if (stageName == "Upload to Nexus") {
     sh "${mavenCMD} deploy"
+  }
+  else {
+    echo "Unknown stage: ${stageName}"
   }
 }
