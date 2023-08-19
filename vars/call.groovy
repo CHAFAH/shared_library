b4bc1e0443d17485637fae278453a4b13cfaa1b5
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-08-19 15:13:14 +0200

    Update call.groovy

--- vars/call.groovy
+++ vars/call.groovy
@@ -1,25 +1,5 @@
-def call(String stageName){
-
-  if ("${stageName}" == "Clone")
-  {
-    sh "git clone https://github.com/CHAFAH/SPRINGAPP-PROJECT.git"
-  }
-  else if ("${stageName}" == "Build")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
-    sh "${mavenCMD} clean package"
-  }
-  else if ("${stageName}" == "SonarQube Report")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
-    sh "${mavenCMD} sonar:sonar"
-  }
-  else if ("${stageName}" == "Upload to Nexus")
-  {
-    def mavenHome = tool name: "maven3.9.4", type: "maven"
-    def mavenCMD = "${mavenHome}/bin/mvn"
-    sh "${mavenCMD} deploy"
-  }
+def call() {
+  sh "echo hello world"
 }
+
+
