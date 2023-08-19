8226f6ff94cdf5cca9a102e202b74c800d1f84fd
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-08-19 15:48:42 +0200

    Update echoMessage.groovy

--- vars/echoMessage.groovy
+++ vars/echoMessage.groovy
@@ -1,3 +1,5 @@
-def call(String message) {
-    echo message
+def mavenHome = tool name: "Maven-3.9.3", type: "maven"
+def mavenCMD = "${mavenHome}/bin/mvn"
+def call() {
+    sh "${mavenCMD} clean package"
 }
