d13e905ae3508cb22358bae222601d00efd1588b
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-08-21 19:18:29 +0200

    Update joint.groovy

--- vars/joint.groovy
+++ vars/joint.groovy
@@ -2,7 +2,7 @@ def call(String stageName) {
     if ("${stageName}" == 'MAVEN BUILD') {
         sh 'mvn clean package'
     }
-    else if ("${stageName}" == 'CODE_COVERAGE') {
-        sh 'mvn sonar:sonar'
+    else if ("${stageName}" == 'DEPLOY_ARTIFACTS') {
+        sh 'mvn deploy'
     }
 }
