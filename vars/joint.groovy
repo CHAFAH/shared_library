cafed96a78ec2e4d8d443f5097bb71091544c698
Prince Chafah Forchu Sani <125821852+CHAFAH@users.noreply.github.com>
2023-08-21 18:09:11 +0200

    Update joint.groovy

--- vars/joint.groovy
+++ vars/joint.groovy
@@ -2,13 +2,4 @@ def call(String stageName) {
     if ("${stageName}" == 'MAVEN BUILD') {
         sh 'mvn clean package'
     }
-    else if ("${stageName}" == 'DEPLOY_ARTIFACTS') {
-        sh 'mvn deploy'
-    }
-    else if ("${stageName}" == 'Authorization') {
-         timeout(time: 48, unit: 'HOURS') {
-                    // some block
-                    input message: 'Approve or Decline'
-                    }
-    }
 }
